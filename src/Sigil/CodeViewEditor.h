6a9c14569bd7e7132928231620720b5e0799fe29
Strahinja Markovic <Strahinja Markovic@localhost>
2009-08-21 01:36:49 +0200

    Added tag 0.1.3 for changeset f3c65920527b

---
+++ src/Sigil/CodeViewEditor.h
@@ -0,0 +1,168 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic, Nokia Corporation
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#pragma once
+#ifndef CODEVIEWEDITOR_H
+#define CODEVIEWEDITOR_H
+
+#include <QPlainTextEdit>
+#include "ViewEditor.h"
+
+class QPaintEvent;
+class QResizeEvent;
+class QSize;
+class QWidget;
+class QPrinter;
+class LineNumberArea;
+
+
+class CodeViewEditor : public QPlainTextEdit, public ViewEditor
+{
+    Q_OBJECT
+
+public:
+
+    // Constructor;
+    // the parameters is the object's parent
+    CodeViewEditor( QWidget *parent = 0 );
+
+    // Sets the content of the View to the specified book
+    void SetBook( const Book &book );
+
+    // Paints the line number area;
+    // receives the event directly 
+    // from the area's paintEvent() handler
+    void LineNumberAreaPaintEvent( QPaintEvent *event );
+
+    // Returns the width the LinuNumberArea
+    // should take (in pixels)
+    int CalculateLineNumberAreaWidth();
+
+    // Returns a list of elements representing a "chain"
+    // or "walk" through the XHTML document with which one
+    // can identify a single element in the document.
+    // This list identifies the element in which the 
+    // keyboard caret is currently located.
+    QList< ViewEditor::ElementIndex > GetCaretLocation(); 
+
+    // Accepts a list returned by a view's GetCaretLocation
+    // and creates and stores an update that sends the caret
+    // in this view to the specified element.
+    // The CodeView implementation initiates the update in
+    // the paint event handler.
+    void StoreCaretLocationUpdate( const QList< ViewEditor::ElementIndex > &hierarchy );
+
+public slots:
+
+    // The base class implementation of the print()
+    // method is not a slot, and we need it as a slot
+    // for print preview support; so this is just
+    // a slot wrapper around that function 
+    void print( QPrinter* printer );
+
+protected:
+
+    // Overridden because after updating itself on a resize event,
+    // the editor needs to update its line number area too
+    void resizeEvent( QResizeEvent *event );
+
+    // Overridden because we need to update the cursor
+    // location if a cursor update (from BookView) 
+    // is waiting to be processed
+    void paintEvent( QPaintEvent *event );
+
+    // Overridden because we want the ExecuteCaretUpdate()
+    // to be called from here when the user clicks inside
+    // this widget in SplitView. Leaving it up to the paint
+    // event handler causes graphical artifacts for SplitView.
+    // So in those conditions, this handler beats the paint one to the update.
+    void mousePressEvent( QMouseEvent *event );
+
+private slots:
+
+    // Called whenever the number of lines changes;
+    // sets a margin where the line number area can be displayed
+    void UpdateLineNumberAreaMargin();
+
+    // The first parameter represents the area 
+    // that the editor needs an update of, and the second
+    // is the amount of pixels the viewport is vertically scrolled
+    void UpdateLineNumberArea( const QRect &rectangle, int vertical_delta );
+
+    // Highlights the line the user is editing
+    void HighlightCurrentLine();    
+
+private:
+
+    // Specifies the lines and characters the caret will
+    // need to move to get to the required position
+    struct CaretMove
+    {
+        // The vertical lines from
+        // the start of the document
+        int vertical_lines;
+
+        // The number of horizontal characters
+        // on the destination line
+        int horizontal_chars;
+
+        CaretMove( int vertical, int horizontal ) 
+            : vertical_lines( vertical ), 
+              horizontal_chars( horizontal ) {}
+    };
+
+    // An element on the stack when searching for
+    // the current caret location. 
+    struct StackElement
+    {
+        // The tag name
+        QString name;
+
+        // The number of child elements
+        // detected for the element, so far.
+        int num_children;
+    };
+
+    // Returns a stack of elements representing the
+    // current location of the caret in the document.
+    // Accepts the number of characters to the end of
+    // the start tag of the element the caret is residing in. 
+    QList< StackElement > GetCaretLocationStack( int offset );
+
+    // Converts the stack provided by GetCaretLocationStack()
+    // and converts it into the element location hierarchy
+    QList< ElementIndex > ConvertStackToHierarchy( const QList< StackElement > stack );
+
+    // Executes the caret updating code
+    // if such an update is pending;
+    // returns true if update was performed
+    bool ExecuteCaretUpdate();
+
+    // Stores the update for the caret location
+    // when switching from BookView to CodeVIew
+    CaretMove m_CaretLocationUpdate;
+
+    // The line number area widget of the code view
+    LineNumberArea *m_LineNumberArea;
+};
+
+#endif // CODEVIEWEDITOR_H
+
---
+++ src/Sigil/ImportHTML.cpp
@@ -0,0 +1,316 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#include "stdafx.h"
+#include "ImportHTML.h"
+#include "Utility.h"
+#include "CleanSource.h"
+#include <QDomDocument>
+#include "XHTMLDoc.h"
+
+static const QString ENTITY_SEARCH = "<!ENTITY\\s+(\\w+)\\s+\"([^\"]+)\">";
+
+
+// Constructor;
+// The parameter is the file to be imported
+ImportHTML::ImportHTML( const QString &fullfilepath )
+    : ImportTXT( fullfilepath )
+{
+
+}
+
+// Reads and parses the file 
+// and returns the created Book
+Book ImportHTML::GetBook()
+{
+    if ( !Utility::IsFileReadable( m_FullFilePath ) )
+
+        return Book();
+
+    LoadSource(); 
+
+    StripFilesFromAnchors();
+    LoadFolderStructure();
+
+    m_Book.source = CleanSource::Clean( m_Book.source );
+
+    return m_Book;
+}
+
+
+// Returns a style tag created 
+// from the provided path to a CSS file
+QString ImportHTML::CreateStyleTag( const QString &fullfilepath )
+{
+    QString source    = Utility::ReadUnicodeTextFile( fullfilepath ); 
+    QString style_tag = "";
+
+    if ( QFileInfo( fullfilepath ).suffix() == "css" )
+    {
+        style_tag = "<style type=\"text/css\">\n" + source + "\n</style>\n";
+    }
+
+    else // XPGT stylesheet
+    {
+        style_tag = "<style type=\"application/vnd.adobe-page-template+xml\">\n" + source + "\n</style>\n";
+    }
+
+    return style_tag;
+}
+
+
+// Updates all references to the resource specified with oldpath
+// to the path of the new resource specified with newpath
+void ImportHTML::UpdateReferences( const QString &oldpath, const QString &newpath )
+{
+    QString filename = QFileInfo( oldpath ).fileName();
+
+    QRegExp reference;
+
+    // Fonts get searched for differently than the other resources
+    if ( ( filename.contains( ".ttf" ) ) || ( filename.contains( ".otf" ) ) )
+
+        reference = QRegExp( "src:\\s*\\w+\\(([^\\)]*/" + QRegExp::escape( filename ) + "|" + QRegExp::escape( filename ) + ")\\)" );
+
+    else
+
+        reference = QRegExp( "<[^>]*\"([^\">]*/" + QRegExp::escape( filename ) + "|" + QRegExp::escape( filename ) + ")\"[^>]*>" );
+
+    int index = -1;
+
+    while ( true )
+    {
+        int newindex = m_Book.source.indexOf( reference );
+
+        // We need to make sure we don't end up
+        // replacing the same thing over and over again
+        if ( ( index == newindex ) || ( newindex == -1 ) )
+
+            break;
+
+        m_Book.source.replace( reference.cap( 1 ), newpath );
+
+        index = newindex;
+    }
+}
+
+
+// Resolves custom ENTITY declarations
+QString ImportHTML::ResolveCustomEntities( const QString &html_source )
+{
+    QString source = html_source;
+
+    QRegExp entity_search( ENTITY_SEARCH );
+
+    QHash< QString, QString > entities;
+
+    int main_index = 0;
+
+    // Catch all custom entity declarations...
+    while ( true )
+    {
+        main_index = source.indexOf( entity_search, main_index );
+
+        if ( main_index == -1 )
+
+            break;
+
+        entities[ "&" + entity_search.cap( 1 ) + ";" ] = entity_search.cap( 2 );
+
+        // Erase the entity declaration
+        source.replace( entity_search.cap( 0 ), "" );
+    }
+
+    // ...and now replace all occurrences
+    foreach( QString key, entities.keys() )
+    {
+        source.replace( key, entities[ key ] );
+    }
+
+    // Clean up what's left of the custom entity declaration field
+    source.replace( QRegExp( "\\[\\s*\\]>" ), "" );
+
+    return source;
+}
+
+
+// Strips the file specifier on all the href attributes 
+// of anchor tags with filesystem links with fragment identifiers;
+// thus something like <a href="chapter01.html#firstheading" />
+// becomes just <a href="#firstheading" />
+void ImportHTML::StripFilesFromAnchors()
+{
+    QDomDocument document;
+ 
+    // We need to clean the source first because
+    // QDomDocument only accepts valid XML
+    document.setContent( CleanSource::Clean( m_Book.source ) );
+
+    QDomNodeList anchors = document.elementsByTagName( "a" );
+
+    for ( int i = 0; i < anchors.count(); i++ )
+    {
+        QDomElement element = anchors.at( i ).toElement();
+
+        // We strip the file specifier on all
+        // the filesystem links with fragment identifiers
+        if (    element.hasAttribute( "href" ) &&
+                QUrl( element.attribute( "href" ) ).isRelative() &&
+                element.attribute( "href" ).contains( "#" )
+           )
+        {
+            element.setAttribute( "href", "#" + element.attribute( "href" ).split( "#" )[ 1 ] );            
+        } 
+    }
+
+    // We also remove the XML carriage returns ("&#xD" sequences)
+    // that the toString() method creates
+    m_Book.source = document.toString().replace( "&#xd;", "" );        
+}
+
+
+// Loads the source code into the Book
+void ImportHTML::LoadSource()
+{
+    QFile file( m_FullFilePath );
+
+    // Check if we can open the file
+    if ( !file.open( QFile::ReadOnly | QFile::Text ) )
+    {
+        QMessageBox::warning(	0,
+            QObject::tr( "Sigil" ),
+            QObject::tr( "Cannot read file %1:\n%2." )
+            .arg( m_FullFilePath )
+            .arg( file.errorString() ) 
+            );
+        return;
+    }
+
+    QByteArray data = file.readAll();
+
+    // This is a workaround for a bug in QTextCodec which
+    // expects the 'charset' attribute to always come after
+    // the 'http-equiv' attribute
+    QString ascii_data = data;
+    ascii_data.replace( QRegExp( "<\\s*meta([^>]*)http-equiv=\"Content-Type\"([^>]*)>" ),
+                        "<meta http-equiv=\"Content-Type\" \\1 \\2>" );
+
+    // Qt docs say Qt will take care of deleting
+    // any QTextCodec objects on application exit
+    m_Book.source = QTextCodec::codecForHtml( ascii_data.toAscii(), QTextCodec::codecForName( "UTF-8" ) )->toUnicode( data );
+    m_Book.source = ResolveCustomEntities( m_Book.source );
+}
+
+
+// Loads the referenced files into the main folder of the book;
+// as the files get a new name, the references are updated
+void ImportHTML::LoadFolderStructure()
+{
+    LoadImages();
+    LoadStyleFiles();  
+}
+
+
+// Loads the images into the book;
+// all references are updated.
+void ImportHTML::LoadImages()
+{
+    QList< QDomNode > image_nodes = XHTMLDoc::GetTagsInDocument( m_Book.source, "img" );
+
+    QStringList image_links;
+
+    // Get a list of all images referenced
+    foreach( QDomNode node, image_nodes )
+    {
+        QDomElement element = node.toElement();
+        QString src         = element.attribute( "src" );
+
+        if ( !src.isEmpty() )
+
+            image_links << src;
+    }
+
+    // Remove duplicate references
+    image_links.removeDuplicates();
+
+    // Load the images into the book and
+    // update all references with new urls
+    foreach( QString image_link, image_links )
+    {
+        QDir folder( QFileInfo( m_FullFilePath ).absoluteDir() );
+
+        QString fullfilepath = QFileInfo( folder, QUrl( image_link ).toString() ).absoluteFilePath();
+        QString newpath      = "../" + m_Book.mainfolder.AddContentFileToFolder( fullfilepath );
+
+        UpdateReferences( image_link, newpath );        
+    }
+
+}
+
+
+// Loads CSS files from 
+// link tags to style tags
+void ImportHTML::LoadStyleFiles()
+{
+    QDomDocument document;
+    document.setContent( m_Book.source );
+
+    QDomNodeList link_nodes = document.elementsByTagName( "link" );
+
+    QStringList style_tags;
+
+    // Get all the style files references in link tags
+    // and convert them into style tags
+    for ( int i = 0; i < link_nodes.count(); i++ )
+    {
+        QDir folder( QFileInfo( m_FullFilePath ).absoluteDir() );
+
+        QDomElement element = link_nodes.at( i ).toElement();
+
+        QFileInfo file_info( folder, QUrl( element.attribute( "href" ) ).toString() );
+
+        if (    file_info.suffix().toLower() == "css" ||
+                file_info.suffix().toLower() == "xpgt"
+           )
+        {
+            style_tags << CreateStyleTag( file_info.absoluteFilePath() );
+        }
+    }
+
+    QDomNode head = document.elementsByTagName( "head" ).at( 0 );
+
+    // Remove the link tags
+    while ( !link_nodes.isEmpty() )
+    {
+        head.removeChild( link_nodes.at( 0 ) );        
+    }
+
+    QString new_source = document.toString().replace( "&#xd;", "" );
+
+    // Paste the new style tags into the head section
+    foreach( QString style, style_tags )
+    {
+        new_source.replace( QRegExp( "(</\\s*(?:head|HEAD)[^>]*>)" ), style + "\n\\1" );
+    }  
+
+    m_Book.source = new_source;
+}
+
