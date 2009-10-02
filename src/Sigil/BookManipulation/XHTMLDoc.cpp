a7c2486c371b19279b4e7f63918c1130acffb130
Strahinja Markovic <Strahinja Markovic@localhost>
2009-10-02 23:38:08 +0200

    Linux installer now creates desktop and 'start menu' shortcuts

---
+++ src/Sigil/BookManipulation/XHTMLDoc.cpp
@@ -0,0 +1,108 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#include <stdafx.h>
+#include "../BookManipulation/XHTMLDoc.h"
+#include <QDomDocument>
+
+static const QString HEAD_ELEMENT = "<\\s*(?:head|HEAD)[^>]*>(.*)</\\s*(?:head|HEAD)[^>]*>";
+
+
+// Returns a list of QDomNodes representing all
+// the elements of the specified tag name
+// in the head section of the provided XHTML source code
+QList< QDomNode > XHTMLDoc::GetTagsInHead( const QString &source, const QString &tag_name )
+{
+    QRegExp head_element( HEAD_ELEMENT );
+
+    source.contains( head_element );
+
+    QDomDocument document;
+    document.setContent( head_element.cap( 0 ) );
+   
+    return DeepCopyNodeList( document.elementsByTagName( tag_name ) );
+}
+
+
+// Returns a list of QDomNodes representing all
+// the elements of the specified tag name
+// in the entire document of the provided XHTML source code
+QList< QDomNode > XHTMLDoc::GetTagsInDocument( const QString &source, const QString &tag_name )
+{
+    QDomDocument document;
+    document.setContent( source );
+
+    return DeepCopyNodeList( document.elementsByTagName( tag_name ) );
+}
+
+
+// We need to remove the XML carriage returns ("&#xD" sequences)
+// that the default toString() method creates so we wrap it in this function
+QString XHTMLDoc::GetQDomNodeAsString( const QDomNode &node )
+{
+    // This function used to be just this one line:
+    //
+    //    return document.toString().replace( "&#xd;", "" );
+    //
+    // But Qt has a bug with the toString() method if the XML
+    // encoding is specified as "us-ascii"... so we work around it.
+
+    QString document_text;
+    QTextStream stream( &document_text );
+    stream.setCodec( "UTF-8" );
+
+    node.save( stream, 1, QDomNode::EncodingFromTextStream );
+
+    return document_text.replace( "&#xd;", "" );   
+}
+
+
+// Removes all the children of a node and
+// returns that same node back.
+// (QDomNodes objects are internally references)
+QDomNode XHTMLDoc::RemoveChildren( QDomNode node )
+{
+    QDomNodeList children = node.childNodes();
+
+    while ( !children.isEmpty() )
+    {
+        node.removeChild( children.at( 0 ) );       
+    }
+
+    return node;
+}
+
+
+// Returns a list of deeply copied QDomNodes
+// from the specified QDomNodeList
+QList< QDomNode > XHTMLDoc::DeepCopyNodeList( QDomNodeList node_list )
+{
+    QList< QDomNode > new_node_list;
+
+    for ( int i = 0; i < node_list.count(); i++ )
+    {
+        new_node_list.append( node_list.at( i ).cloneNode() );
+    }
+
+    return new_node_list;
+}
+
+
---
+++ src/Sigil/Importers/ImportSGF.h
@@ -0,0 +1,53 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#pragma once
+#ifndef IMPORTSGF_H
+#define IMPORTSGF_H
+
+#include "ImportEPUB.h"
+
+class ImportSGF : public ImportEPUB
+{
+
+public:
+
+    // Constructor;
+    // The parameter is the file to be imported
+    ImportSGF( const QString &fullfilepath );
+
+    // Reads and parses the file 
+    // and returns the created Book;
+    // Overrides;
+    Book GetBook();
+
+private:
+
+    // Loads the source code into the Book
+    void LoadSource();
+
+    // Loads the referenced files into the main folder of the book
+    void LoadFolderStructure();
+
+};
+
+#endif // IMPORTSGF_H
+
