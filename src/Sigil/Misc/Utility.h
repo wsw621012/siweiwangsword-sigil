5489e1221ca248e8159aa0bb161178edb54e9321
Strahinja Markovic <Strahinja Markovic@localhost>
2009-10-13 04:17:33 +0200

    Adding -03 flag for GCC... I can't belive this was off.

---
+++ src/Sigil/Misc/Utility.h
@@ -0,0 +1,105 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#pragma once
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <QString>
+#include <QDir>
+
+class QDomNode;
+
+class Utility
+{	
+
+public:
+
+    // Returns a random string of length characters
+    static QString GetRandomString( int length );
+
+    // Returns a substring of a specified string;
+    // the characters included are in the interval: 
+    // [ start_index, end_index >
+    static QString Substring( int start_index, int end_index, const QString &string );
+
+    // Replace the first occurrence of string "before"
+    // with string "after" in string "string"
+    static QString ReplaceFirst( const QString &before, const QString &after, const QString &string );
+   
+    // Copies every file and folder in the source folder 
+    // to the destination folder; the paths to the folders are submitted;
+    // the destination folder needs to be created in advance
+    static void CopyFiles( const QString &fullfolderpath_source, const QString &fullfolderpath_destination );
+
+    // Deletes the folder specified with fullfolderpath
+    // and all the files (and folders, recursively) in it
+    static void DeleteFolderAndFiles( const QString &fullfolderpath );  
+
+    // Returns the full path to a new temporary folder;
+    // the caller is responsible for creating and deleting the folder
+    static QString GetNewTempFolderPath(); 
+
+    // Creates a copy of the provided file with a random name in
+    // the systems TEMP directory and returns the full path to the new file.
+    // The extension of the original file is preserved. If the original file
+    // doesn't exist, an empty string is returned.
+    static QString CreateTemporaryCopy( const QString &fullfilepath ); 
+
+    // Returns true if the file can be read;
+    // shows an error dialog if it can't
+    // with a message elaborating what's wrong
+    static bool IsFileReadable( const QString &fullfilepath );
+
+    // Reads the text file specified with the full file path;
+    // text needs to be in UTF-8 or UTF-16; if the file cannot
+    // be read, an error dialog is shown and an empty string returned
+    static QString ReadUnicodeTextFile( const QString &fullfilepath );
+
+    // Writes the provided text variable to the specified
+    // file; if the file exists, it is truncated
+    static void WriteUnicodeTextFile( const QString &text, const QString &fullfilepath );
+
+    // Returns a value for the environment variable name passed;
+    // if the env var isn't set, it returns an empty string
+    static QString GetEnvironmentVar( const QString &variable_name );
+    
+    // Accepts a string with HTML and returns the text
+    // in that HTML fragment. For instance 
+    //   <h1>Hello <b>Qt</b> <![CDATA[<xml is cool>]]></h1>
+    // returns
+    //   Hello Qt <xml is cool>
+    static QString GetTextInHtml( const QString &source );
+
+    // Resolves HTML entities in the provided string.
+    // For instance: 
+    //    Bonnie &amp; Clyde
+    // returns
+    //    Bonnie & Clyde
+    static QString ResolveHTMLEntities( const QString &text );
+
+    // Returns the same number, but rounded to one decimal place
+    static float RoundToOneDecimal( float number );
+};
+
+#endif // UTILITY_H
+
+
---
+++ src/Sigil/ViewEditors/BookViewEditor.h
@@ -0,0 +1,142 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#pragma once
+#ifndef BOOKVIEWEDITOR_H
+#define BOOKVIEWEDITOR_H
+
+#include <QWebView>
+#include "ViewEditor.h"
+
+class BookViewEditor : public QWebView, public ViewEditor
+{
+    Q_OBJECT
+
+public:
+
+    // Constructor;
+    // the parameters is the object's parent
+    BookViewEditor( QWidget *parent = 0 );
+
+    // Sets the content of the View to the specified book
+    void SetBook( const Book &book );
+
+    // Executes the specified command on the document with javascript
+    void ExecCommand( const QString &command );
+
+    // Executes the specified command with the specified parameter
+    // on the document with javascript
+    void ExecCommand( const QString &command, const QString &parameter );
+
+    // Returns the state of the JavaScript command provided
+    bool QueryCommandState( const QString &command );
+
+    // Implements the "formatBlock" execCommand because
+    // WebKit's default one has bugs.
+    // It takes an element name as an argument (e.g. "p"),
+    // and replaces the element the cursor is located in with it.
+    void FormatBlock( const QString &element_name );
+
+    // Returns the name of the element the caret is located in;
+    // if text is selected, returns the name of the element
+    // where the selection *starts*
+    QString GetCaretElementName();
+
+    // Returns a list of elements representing a "chain"
+    // or "walk" through the XHTML document with which one
+    // can identify a single element in the document.
+    // This list identifies the element in which the 
+    // keyboard caret is currently located.
+    QList< ViewEditor::ElementIndex > GetCaretLocation(); 
+
+    // Accepts a list returned by a view's GetCaretLocation
+    // and creates and stores an update that sends the caret
+    // in this view to the specified element.
+    // The BookView implementation initiates the update in
+    // the JavascriptOnDocumentLoad() function.
+    void StoreCaretLocationUpdate( const QList< ViewEditor::ElementIndex > &hierarchy );
+
+    // Sets a zoom factor for the view,
+    // thus zooming in (factor > 1.0) or out (factor < 1.0). 
+    void SetZoomFactor( float factor );
+
+    // Returns the View's current zoom factor
+    float GetZoomFactor() const;
+
+signals:
+
+    // The identically named QWebPage signal is wired to this one,
+    // and we also emit it ourselves when necessary.
+    void textChanged();
+
+    // Emitted whenever the zoom factor changes
+    void ZoomFactorChanged( float new_zoom_factor );
+
+private slots:
+
+    // Executes javascript that needs to be run when
+    // the document has finished loading
+    void JavascriptOnDocumentLoad();
+
+    // Updates the state of the m_isLoadFinished variable
+    // depending on the received loading progress; if the 
+    // progress equals 100, the state is true, otherwise false.
+    void UpdateFinishedState( int progress );
+
+private:
+
+    // Evaluates the provided javascript source code 
+    // and returns the result of the last executed javascript statement
+    QVariant EvaluateJavascript( const QString &javascript );
+
+    // Executes the caret updating code
+    // if an update is pending;
+    // returns true if update was performed
+    bool ExecuteCaretUpdate();
+
+
+    ///////////////////////////////
+    // PRIVATE MEMBER VARIABLES
+    ///////////////////////////////
+
+    // The javascript source code of the jQuery library
+    const QString c_JQuery;
+
+    // The javascript source code of the jQuery
+    // ScrollTo extension library
+    const QString c_JQueryScrollTo;
+
+    // The javascript source code used
+    // to get a hierarchy of elements from
+    // the caret element to the top of the document
+    const QString c_GetCaretLocation;
+
+    // The javascript source code for the 
+    // caret update when switching from 
+    // CodeView to BookView
+    QString m_CaretLocationUpdate;
+
+    bool m_isLoadFinished;
+};
+
+
+#endif // BOOKVIEWEDITOR_H
+
