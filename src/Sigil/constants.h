52781cfa2e5844f7618c1fd3ffd2ae322f0d48ac
Strahinja Markovic <Strahinja Markovic@localhost>
2009-12-07 04:16:04 +0100

    Backed out changeset 759b07fe47bc

---
+++ src/Sigil/constants.h
@@ -0,0 +1,37 @@
+/************************************************************************
+**
+**  Copyright (C) 2009  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+class QString;
+
+// These enable us to use constants defined
+// in one CPP file to be used in another
+extern const QString BODY_START;
+extern const QString BODY_END;
+extern const QString HEAD_END;
+extern const QString BREAK_TAG_SEARCH;
+extern const QString BREAK_TAG_INSERT;
+extern const QString HEADING;
+extern const QString STYLE_TAG;
+extern const QString WIN_PATH_SUFFIX;
+extern const QString NIX_PATH_SUFFIX;
+extern const QString VERSION_NUMBERS;
+extern const int PROGRESS_BAR_MINIMUM_DURATION;
+
