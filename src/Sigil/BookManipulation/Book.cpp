6307f2def2acc3adc895085c4563bf2cb012878e
Strahinja Markovic <Strahinja Markovic@localhost>
2010-05-04 12:54:11 +0200

    Fixed an issue with the ReplaceAll across files not informing the opened tabs of the change

---
+++ src/Sigil/BookManipulation/Book.cpp
@@ -0,0 +1,288 @@
+/************************************************************************
+**
+**  Copyright (C) 2009, 2010  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#include <stdafx.h>
+#include "../BookManipulation/Book.h"
+#include "../BookManipulation/XHTMLDoc.h"
+#include "../Misc/Utility.h"
+#include "ResourceObjects/HTMLResource.h"
+#include "../BookManipulation/CleanSource.h"
+#include "../SourceUpdates/AnchorUpdates.h"
+#include "../SourceUpdates/PerformHTMLUpdates.h"
+#include "../SourceUpdates/AnchorUpdates.h"
+#include <QDomDocument>
+
+static const QString FIRST_CSS_NAME   = "Style0001.css";
+static const QString PLACEHOLDER_TEXT = "PLACEHOLDER";
+static const QString EMPTY_HTML_FILE  = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                                        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n"
+                                        "    \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n\n"							
+                                        "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+                                        "<head>\n"
+                                        "<title></title>\n"
+                                        "</head>\n"
+                                        "<body>\n"
+
+                                        // The "nbsp" is here so that the user starts writing
+                                        // inside the <p> element; if it's not here, webkit
+                                        // inserts text _outside_ the <p> element
+                                        "<p>&nbsp;</p>\n"
+                                        "</body>\n"
+                                        "</html>";
+
+
+Book::Book()
+    : 
+    m_PublicationIdentifier( Utility::CreateUUID() ),
+    m_IsModified( false )
+{
+   
+}
+
+
+QUrl Book::GetBaseUrl() const
+{
+    return QUrl::fromLocalFile( m_Mainfolder.GetFullPathToTextFolder() + "/" );
+}
+
+
+
+FolderKeeper& Book::GetFolderKeeper()
+{
+    return m_Mainfolder;
+}
+
+
+const FolderKeeper& Book::GetConstFolderKeeper()
+{
+    return m_Mainfolder;
+}
+
+
+QString Book::GetPublicationIdentifier() const
+{
+    return m_PublicationIdentifier;
+}
+
+
+QHash< QString, QList< QVariant > > Book::GetMetadata() const
+{
+    return m_Metadata;
+}
+
+
+void Book::SetMetadata( const QHash< QString, QList< QVariant > > metadata )
+{
+    m_Metadata = metadata;
+    SetModified( true );
+}
+
+
+HTMLResource& Book::CreateNewHTMLFile()
+{
+    QString folderpath = Utility::GetNewTempFolderPath();
+    QDir dir( folderpath );
+    dir.mkpath( folderpath );
+
+    QString fullfilepath = folderpath + "/" + m_Mainfolder.GetUniqueFilenameVersion( FIRST_CHAPTER_NAME );
+    int reading_order = m_Mainfolder.GetHighestReadingOrder() + 1;
+
+    Utility::WriteUnicodeTextFile( PLACEHOLDER_TEXT, fullfilepath );
+
+    HTMLResource &html_resource = *qobject_cast< HTMLResource* >( 
+                                        &m_Mainfolder.AddContentFileToFolder( fullfilepath, reading_order ) );
+
+    QtConcurrent::run( Utility::DeleteFolderAndFiles, dir.absolutePath() );
+
+    SetModified( true );
+    return html_resource;
+}
+
+
+void Book::CreateEmptyHTMLFile()
+{
+    QDomDocument document;
+    XHTMLDoc::LoadTextIntoDocument( EMPTY_HTML_FILE, document );
+
+    CreateNewHTMLFile().SetDomDocument( document );
+    SetModified( true );
+}
+
+
+void Book::CreateEmptyCSSFile()
+{
+    QString folderpath = Utility::GetNewTempFolderPath();
+    QDir dir( folderpath );
+    dir.mkpath( folderpath );
+
+    QString fullfilepath = folderpath + "/" + m_Mainfolder.GetUniqueFilenameVersion( FIRST_CSS_NAME );
+
+    Utility::WriteUnicodeTextFile( "", fullfilepath );
+
+    m_Mainfolder.AddContentFileToFolder( fullfilepath );
+
+    QtConcurrent::run( Utility::DeleteFolderAndFiles, dir.absolutePath() );
+    SetModified( true );
+}
+
+
+HTMLResource& Book::CreateChapterBreakOriginalResource( const QString &content, HTMLResource& originating_resource )
+{
+    const QString &originating_filename = originating_resource.Filename();
+
+    originating_resource.RenameTo( m_Mainfolder.GetUniqueFilenameVersion( FIRST_CHAPTER_NAME ) );
+
+    int reading_order = originating_resource.GetReadingOrder();
+    Q_ASSERT( reading_order >= 0 );
+
+    QList< HTMLResource* > html_resources = m_Mainfolder.GetResourceTypeList< HTMLResource >( true );
+
+    // We need to "make room" for the reading order of the new resource
+    for ( int i = reading_order; i < html_resources.count(); ++i )
+    {
+        HTMLResource* resource = html_resources[ i ];
+        resource->SetReadingOrder( resource->GetReadingOrder() + 1 );
+    }
+
+    HTMLResource &html_resource = CreateNewHTMLFile();
+    html_resource.RenameTo( originating_filename );
+
+    QDomDocument document;
+    XHTMLDoc::LoadTextIntoDocument( CleanSource::Clean( content ), document );
+    html_resource.SetDomDocument( document );
+
+    html_resource.SetReadingOrder( reading_order );
+
+    // We can just append this since we don't need
+    // them in sorted order for the updates.
+    html_resources.append( &html_resource );
+    AnchorUpdates::UpdateAllAnchorsWithIDs( html_resources );
+
+    SetModified( true );
+    return html_resource;
+}
+
+void Book::CreateNewChapters( const QStringList& new_chapters )
+{
+    CreateNewChapters( new_chapters, QHash< QString, QString >() );
+    SetModified( true );
+}
+
+
+void Book::CreateNewChapters( const QStringList& new_chapters,
+                              const QHash< QString, QString > &html_updates )
+{
+    if ( new_chapters.isEmpty() )
+
+        return;
+
+    QDir dir( Utility::GetNewTempFolderPath() );
+    dir.mkpath( dir.absolutePath() );
+    QString folderpath = dir.absolutePath();
+
+    QFutureSynchronizer< void > sync;
+
+    int next_reading_order = m_Mainfolder.GetHighestReadingOrder() + 1;
+
+    for ( int i = 0; i < new_chapters.count(); ++i )
+    {
+        int reading_order = next_reading_order + i;
+
+        sync.addFuture( 
+            QtConcurrent::run( 
+                this, &Book::CreateOneNewChapter, new_chapters.at( i ), reading_order, folderpath, html_updates ) );
+    }	
+
+    sync.waitForFinished();
+
+    QtConcurrent::run( Utility::DeleteFolderAndFiles, folderpath );
+
+    AnchorUpdates::UpdateAllAnchorsWithIDs( m_Mainfolder.GetResourceTypeList< HTMLResource >() );
+    SetModified( true );
+}
+
+
+void Book::SaveAllResourcesToDisk()
+{
+    QList< Resource* > resources =  m_Mainfolder.GetResourceList();
+    QtConcurrent::blockingMap( resources, SaveOneResourceToDisk );
+}
+
+
+bool Book::IsModified() const
+{
+    return m_IsModified;
+}
+
+
+void Book::SetModified( bool modified )
+{
+    bool old_modified_state = m_IsModified;
+    m_IsModified = modified;
+
+    if ( modified != old_modified_state )
+
+        emit ModifiedStateChanged( m_IsModified );
+}
+
+
+void Book::SaveOneResourceToDisk( Resource *resource )
+{
+    resource->SaveToDisk( true );
+}
+
+void Book::CreateOneNewChapter( const QString &source,
+                                int reading_order,
+                                const QString &temp_folder_path )
+{
+    CreateOneNewChapter( source, reading_order, temp_folder_path, QHash< QString, QString >() );
+}
+
+
+void Book::CreateOneNewChapter( const QString &source, 
+                                int reading_order, 
+                                const QString &temp_folder_path,
+                                const QHash< QString, QString > &html_updates )
+{
+    QString filename     = FIRST_CHAPTER_PREFIX + QString( "%1" ).arg( reading_order + 1, 4, 10, QChar( '0' ) ) + ".xhtml";
+    QString fullfilepath = temp_folder_path + "/" + filename;
+
+    Utility::WriteUnicodeTextFile( "PLACEHOLDER", fullfilepath );
+
+    HTMLResource *html_resource = qobject_cast< HTMLResource* >( 
+        &m_Mainfolder.AddContentFileToFolder( fullfilepath, reading_order ) );
+
+    Q_ASSERT( html_resource );
+
+    if ( html_updates.isEmpty() )
+    {
+        QDomDocument document;
+        XHTMLDoc::LoadTextIntoDocument( CleanSource::Clean( source ), document );
+        html_resource->SetDomDocument( document );
+    }
+
+    else
+    {
+        html_resource->SetDomDocument( 
+            PerformHTMLUpdates( CleanSource::Clean( source ), html_updates, QHash< QString, QString >() )() );
+    }    
+}
+
---
+++ src/Sigil/MainUI/BookBrowser.h
@@ -0,0 +1,308 @@
+/************************************************************************
+**
+**  Copyright (C) 2009, 2010  Strahinja Markovic
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#pragma once
+#ifndef BOOKBROWSER_H
+#define BOOKBROWSER_H
+
+#include <QDockWidget>
+#include <QSharedPointer>
+#include "../BookManipulation/Book.h"
+#include "../ResourceObjects/Resource.h"
+
+class QTreeView;
+class OPFModel;
+class HTMLResource;
+class Resource;
+class QModelIndex;
+class QUrl;
+class QPoint;
+class QMenu;
+class QAction;
+class QSignalMapper;
+
+
+/**
+ * Represents the pane with which the user can manipulate the book's files.
+ * The user uses this browser to open the files in tabs. Some files can then
+ * be edited, others cannot.
+ */
+class BookBrowser : public QDockWidget
+{
+    Q_OBJECT
+
+public:
+
+    /**
+     * Constructor.
+     *
+     * @param parent The QObject's parent.
+     */
+    BookBrowser( QWidget *parent = 0 );
+
+    /**
+     * Destructor.
+     */
+    ~BookBrowser();
+
+public slots:
+
+    /**
+     * Sets the book whose contents the browser should display.
+     *
+     * @param book The book to be displayed.
+     */
+    void SetBook( QSharedPointer< Book > book );
+
+    /**
+     * Refreshes the display of the book.
+     * This is done by clearing the current content
+     * and reloading the file list from the book.
+     */
+    void Refresh();
+
+    /**
+     * Opens the HTML resource referenced by the specified URL.
+     *
+     * @param url The URL to open.
+     */
+    void OpenUrlResource( const QUrl &url );
+
+signals:
+
+    /**
+     * Emitted when the user double-clicks a resource in the browser.
+     *
+     * @param resource The clicked resource.
+     */
+    void ResourceDoubleClicked( Resource &resource );
+
+    /**
+     * Emitted when the browser wants a resource to be opened.
+     *
+     * @param resource The resource that should be opened.
+     * @param precede_current_tab Should the new tab precede the currently opened one.
+     * @param fragment The fragment ID to which the new tab should be scrolled.
+     */
+    void OpenResourceRequest( Resource &resource, bool precede_current_tab, const QUrl &fragment );
+
+private slots:
+
+    /**
+     * Emits the ResourceDoubleClicked signal.
+     *
+     * @param The clicked model index in the Tree View.
+     */
+    void EmitResourceDoubleClicked( const QModelIndex &index );
+
+    /**
+     * Opens the context menu at the requested point.
+     *
+     * @param point The point at which the menu should be opened.
+     */
+    void OpenContextMenu( const QPoint &point );
+
+    /**
+     * Implements the Add New context menu action functionality.
+     */
+    void AddNew();
+
+    /**
+    * Implements the Add Existing context menu action functionality.
+    */
+    void AddExisting();
+
+    /**
+    * Implements the Rename context menu action functionality.
+    */
+    void Rename();
+
+    /**
+    * Implements the Add New context menu action functionality.
+    */
+    void Remove();
+
+    /**
+     * Implements the Cover Image semantic context menu action functionality.
+     */
+    void SetCoverImage();
+
+    /**
+     * Adds the semantic type information to the currently
+     * selected resource.
+     *
+     * @param type The integer value of the GuideSemantics::GuideSemanticType
+     *             to be added to the resource.
+     */
+    void AddGuideSemanticType( int type );
+
+private:
+
+    /**
+    * Reads all the stored application settings like
+    * window position, geometry etc.
+    */
+    void ReadSettings();
+
+    /**
+    * Writes all the stored application settings like
+    * window position, geometry etc.
+    */
+    void WriteSettings();
+
+    /**
+     * Performs the initial setup for the tree view. 
+     */
+    void SetupTreeView();
+
+    /**
+     * Creates all the context menu actions.
+     */
+    void CreateContextMenuActions();
+
+    /**
+     * Creates all the actions for the <guide>
+     * element semantics.
+     */
+    void CreateGuideSemanticActions();
+
+    /**
+     * Tries to setup the context menu for the specified point,
+     * and returns true on success.
+     *
+     * @param point The point at which the menu should be opened.
+     * @return \c true if the menu could be set up.
+     */
+    bool SuccessfullySetupContextMenu( const QPoint &point );
+
+    /**
+     * Sets up the sub-menu for adding semantic information.
+     *
+     * @param resource The resource on which the context menu was invoked.
+     */
+    void SetupSemanticContextmenu( Resource *resource );
+
+    /**
+     * Sets up the sub-menu for adding semantic information,
+     * when the menu is invoked for HTMLResources.
+     *
+     * @param resource The resource on which the context menu was invoked.
+     */
+    void SetupHTMLSemanticContextMenu( Resource *resource );
+
+    /**
+     * Sets up the sub-menu for adding semantic information,
+     * when the menu is invoked for ImageResources.
+     *
+     * @param resource The resource on which the context menu was invoked.
+     */
+    void SetupImageSemanticContextMenu( Resource *resource ); 
+
+
+    /**
+     * Sets the checked state for the HTML semantic actions
+     * based on the resource's current state.
+     *
+     * @param resource The resource on which the context menu was invoked.
+     */
+    void SetHTMLSemanticActionCheckState( Resource *resource );
+
+    /**
+     * Returns the currently selected resource in the tree view.
+     *
+     * @return The currently selected resource in the tree view,
+     *         or NULL if no resource is selected. 
+     */
+    Resource* GetCurrentResource();
+
+    /**
+     * Connects all the required signals to their respective slots.
+     */
+    void ConnectSignalsToSlots();
+
+
+    ///////////////////////////////
+    // PRIVATE MEMBER VARIABLES
+    ///////////////////////////////
+
+    /**
+     * The bool currently being displayed.
+     */
+    QSharedPointer< Book > m_Book;
+
+    /**
+     * The tree view used to represent the book's files.
+     */
+    QTreeView &m_TreeView;
+    
+    /**
+     * The data model used to feed the tree view.
+     */
+    OPFModel &m_OPFModel;
+
+    /**
+     * The right-click context menu.
+     */
+    QMenu &m_ContextMenu;
+
+    /**
+     * The sub-menu for adding semantic
+     * information to resources.
+     */
+    QMenu &m_SemanticsContextMenu;
+
+    // The context menu actions.
+
+    QAction *m_AddExisting;
+    QAction *m_AddNew;
+    QAction *m_Rename;
+    QAction *m_Remove;
+    QAction *m_CoverImage;
+
+    /**
+     * All the semantic actions for the <guide>
+     * element. Only present on HTMLResources.
+     */
+    QList< QAction* > m_GuideSemanticActions;
+
+    /**
+     * Used to translate all the triggered() signals from the
+     * various guide-related semantic actions into calls to
+     * AddGuideSemanticType().
+     */
+    QSignalMapper &m_GuideSemanticMapper;
+
+    /**
+     * The resource type of the last item on which the 
+     * the context menu was invoked.
+     */
+    Resource::ResourceType m_LastContextMenuType;
+
+    /**
+     * The last folder from which the user
+     * added an existing file.
+     */
+    QString m_LastFolderOpen;
+};
+
+#endif // BOOKBROWSER_H
+
+
